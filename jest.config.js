513dfb21b32e7c2fefea0e161fa212d12fa1e542
donal <donalspring@gmail.com>
2020-08-04 10:39:48 +0100

    🏂 ADD - Jenkinsfile, Dockerfile and Jest Junit reporting 🤤

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest"
+  preset: "@vue/cli-plugin-unit-jest",
+  collectCoverage: true,
+  coverageReporters: [
+    "text",
+    "lcov"
+  ],
+  coverageDirectory: 'reports/coverage',
+  reporters: ["default", ["jest-junit", {
+    suiteName: "jest tests"
+  }]]
 };
