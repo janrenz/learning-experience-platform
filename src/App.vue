193fec5f062a8b0b54e15a9ef9a3ec12fdbfab77
donal <donalspring@gmail.com>
2020-08-06 17:17:16 +0100

    🐏 ADD - bootstrap and nav bar 🐏

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
-      <button @click="signUp">Sign me Up</button>
-    </div>
+    <NavBar></NavBar>
+
+    <div id="nav"></div>
     <router-view />
   </div>
 </template>
@@ -20,27 +18,15 @@
 
 #nav {
   padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
 
 <script>
+import NavBar from "@/components/NavBar.vue";
+
 export default {
-  methods: {
-    signUp() {
-      this.$keycloak.register({
-        redirectUri: this.$config.SSO.redirectUri,
-        action: "register"
-      });
-    }
+  components: {
+    NavBar
   }
 };
 </script>
