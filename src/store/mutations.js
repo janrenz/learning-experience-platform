c74b785c6c2e8b0066c3fb04d5fd20692f65908f
Mahendran Kannan <gkm09@hotmail.com>
2020-08-10 16:11:05 +0530

    🐳 ADD - added mock topics service, onboarding page and added bade for interests 🐳

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,8 +1,19 @@
+import Vue from "vue";
+
 export default {
   GET_ALL_COURSES(state, courses) {
     state.courses = courses;
   },
   SET_CONFIG(state, config) {
     state.config = config;
+  },
+  GET_ALL_TOPICS(state, topics) {
+    topics.forEach(t => {
+      Vue.set(t, "active", false);
+    });
+    state.topics = topics;
+  },
+  SET_TOPIC(state, topic, index) {
+    Vue.set(state.topics, index, topic);
   }
 };
